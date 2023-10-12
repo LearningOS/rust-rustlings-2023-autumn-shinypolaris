e6306105feb99f088669001d55d8e2c371ed5232
p0lar1s <88910096+shinypolaris@users.noreply.github.com>
2023-10-13 05:41:37 +0800

    update: All exercises completed

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+// I AM DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let sep: Vec<&str> = s.split(",").collect();
+            if sep.len() != 2 {
+                Person::default()
+            } else {
+                let name = sep[0].to_string();
+                let age = sep[1].parse::<usize>();
+                if name.len() == 0 {
+                    Person::default()
+                } else {
+                    match age {
+                        Ok(a) => Person { name, age: a },
+                        Err(_) => Person::default(),
+                    }
+                }
+            }
+        }
     }
 }
 
